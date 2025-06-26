b7abf4915bbaa8252d4f70b2f65dd0c7987250b9
Zau Seng <110077742+Mnaw161329@users.noreply.github.com>
2025-06-26 15:54:56 +0630

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const words = [];
+
+function arrangeWords(){
+    document.getElementById("afterArrange").innerHTML = "";
+
+    const word = document.getElementById("wordsInput").value.trim();
+    if (word === "") {
+        alert("Please enter some words.");
+        return;
+    } else {
+        words.push(word);
+        document.getElementById("wordsInput").value = "";
+    }
+
+    document.getElementById("beforeArrange").innerHTML = words.join(", ");
+
+    const arrangedWords = words.sort((a, b) => 
+    a.toLowerCase().localeCompare(b.toLowerCase())
+    );
+
+    for (let i = 0; i < arrangedWords.length; i++) {
+        const newElement = document.createElement("li");
+        newElement.textContent = arrangedWords[i];
+        document.getElementById("afterArrange").appendChild(newElement);
+    }
+
+};
